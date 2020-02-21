4e54eab924789aadf197f6649934630a6ba46434
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-21 09:37:16 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,10 @@
 class ArtistsController < ApplicationController
 
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+
   private
   def artist_params
     params.require(:artists).permit(:name, :bio)
