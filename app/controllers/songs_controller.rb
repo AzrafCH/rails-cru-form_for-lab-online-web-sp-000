89aee6a28b4cdaa8c0d9b324f5c4445cedb77a15
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-21 09:47:02 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(song_params(:name, :genre_id, :artist_id))
-    redirect_to artist_path(@artist)
+    redirect_to song_path(@song)
   end
 
 
